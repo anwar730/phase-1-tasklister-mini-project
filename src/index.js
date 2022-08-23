b129ca377aa3ba25e6881fde4f470983cf2095c7
Anwar Ismail <anwar.ismail@student.moringaschool.com>
2022-08-23 04:17:41 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+   
+
+   })
+  
+   
+   let form=document.querySelector('form')
+   
+   form.addEventListener('submit',(e)=>{
+    
+      e.preventDefault()
+      
+      buildMyTodo(e.target.new_task_description.value)
+      form.reset()
+   })     
+
+function buildMyTodo(todo){
+  
+  
+  let p=document.createElement('p')
+  
+  
+  p.textContent=`${todo}  `
+  
+  let btn=document.createElement('button')
+  btn.textContent='X'
+
+ 
+ console.log(p)
+  
+  btn.addEventListener('click',(e)=>{
+    e.target.parentNode.remove()
+  })
+ 
+  
+ p.appendChild(btn)
+ document.querySelector('#list').appendChild(p)
+}
+
